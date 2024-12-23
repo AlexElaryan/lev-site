26befaa69d8fec9f18c1b08384d95c3a7a64a034
AlexElaryan <alexelaryan1@gmail.com>
2024-12-23 20:23:19 +0400

    Updates

--- js/contact.js
+++ js/contact.js
@@ -1,40 +1,47 @@
 const result = document.getElementById('result');
 let contactForm = document.getElementById('contactForm');
 
-contactForm.addEventListener('submit', function(e) {
-    const formData = new FormData(contactForm);
+contactForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
+    const formData = new FormData(contactForm);
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
 
-    result.innerHTML = "Please wait..."
+    result.innerHTML = "Please wait...";
 
     fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = json.message;
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function() {
-            contactForm.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 3000);
-        });
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+    })
+    .then(async (response) => {
+        let json = await response.json();
+        if (response.status === 200) {
+            result.innerHTML = json.message;
+
+            // Show success alert
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent successfully!",
+                icon: "success"
+            });
+        } else {
+            console.log(response);
+            result.innerHTML = json.message;
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        result.innerHTML = "Something went wrong!";
+    })
+    .finally(() => {
+        contactForm.reset();
+        setTimeout(() => {
+            result.style.display = "none";
+        }, 3000);
+    });
 });
